38b60c663af43c8e0f8502e31827667af722ac0c
Christine Tsou <cyt3ea@virginia.edu>
2016-10-05 14:43:22 -0400

    Add version api to urls

--- models/webapp/urls.py
+++ models/webapp/urls.py
@@ -18,24 +18,24 @@ from django.contrib import admin
 from webapp import views
 
 urlpatterns = [
-    url(r'^api/users/create$', views.create_user),
-    url(r'^api/users/(\d+)$', views.lookup_user),
-    url(r'^api/users/delete/(\d+)$', views.delete_user),
-    url(r'^api/users/update/(\d+)$', views.update_user),
+    url(r'^api/v1/users/create$', views.create_user),
+    url(r'^api/v1/users/(\d+)$', views.lookup_user),
+    url(r'^api/v1/users/delete/(\d+)$', views.delete_user),
+    url(r'^api/v1/users/update/(\d+)$', views.update_user),
 
-    url(r'^api/hairs/create$', views.create_hair),
-    url(r'^api/hairs/(\d+)$', views.lookup_hair),
-    url(r'^api/hairs/delete/(\d+)$', views.delete_hair),
-    url(r'^api/hairs/update/(\d+)$', views.update_hair),
+    url(r'^api/v1/hairs/create$', views.create_hair),
+    url(r'^api/v1/hairs/(\d+)$', views.lookup_hair),
+    url(r'^api/v1/hairs/delete/(\d+)$', views.delete_hair),
+    url(r'^api/v1/hairs/update/(\d+)$', views.update_hair),
 
-    url(r'^api/reviews/create$', views.create_review),
-    url(r'^api/reviews/(\d+)$', views.lookup_review),
-    url(r'^api/reviews/delete/(\d+)$', views.delete_review),
-    url(r'^api/reviews/update/(\d+)$', views.update_review),
+    url(r'^api/v1/reviews/create$', views.create_review),
+    url(r'^api/v1/reviews/(\d+)$', views.lookup_review),
+    url(r'^api/v1/reviews/delete/(\d+)$', views.delete_review),
+    url(r'^api/v1/reviews/update/(\d+)$', views.update_review),
 
-    url(r'^api/stylists/create$', views.create_stylist),
-    url(r'^api/stylists/(\d+)$', views.lookup_stylist),
-    url(r'^api/stylists/delete/(\d+)$', views.delete_stylist),
-    url(r'^api/stylists/update/(\d+)$', views.update_stylist),
+    url(r'^api/v1/stylists/create$', views.create_stylist),
+    url(r'^api/v1/stylists/(\d+)$', views.lookup_stylist),
+    url(r'^api/v1/stylists/delete/(\d+)$', views.delete_stylist),
+    url(r'^api/v1/stylists/update/(\d+)$', views.update_stylist),
 
 ]
