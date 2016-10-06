58ebc86782a41844cb311116270081dc107d5bfb
Christine Tsou <cyt3ea@virginia.edu>
2016-10-06 16:41:02 -0400

    Changed models; index and detail display better data

--- exp/expservice/views.py
+++ exp/expservice/views.py
@@ -12,6 +12,9 @@ def all_hairs(request):
 		req = urllib.request.Request('http://models-api:8000/api/v1/hairs/all_hairs/')
 		resp_json = urllib.request.urlopen(req).read().decode('utf8')
 		resp = json.loads(resp_json)
+		all_hairs = resp["resp"]["all_hairs"]
+		for hair in all_hairs:
+			getHairFK(hair)
 		return JsonResponse(resp)
 
 def popular_hairs(request):
@@ -21,6 +24,9 @@ def popular_hairs(request):
 		req = urllib.request.Request('http://models-api:8000/api/v1/hairs/popular_hairs/')
 		resp_json = urllib.request.urlopen(req).read().decode('utf8')
 		resp = json.loads(resp_json)
+		popular_hairs = resp["resp"]["popular_hairs"]
+		for hair in popular_hairs:
+			getHairFK(hair)
 		return JsonResponse(resp)
 
 def detail_hair(request, hair_id):
@@ -30,7 +36,24 @@ def detail_hair(request, hair_id):
 		req = urllib.request.Request('http://models-api:8000/api/v1/hairs/' + hair_id + '/')
 		resp_json = urllib.request.urlopen(req).read().decode('utf8')
 		resp = json.loads(resp_json)
+		hair = resp["resp"]
+		getHairFK(hair)
 		return JsonResponse(resp)
+
+def getHairFK(hair):
+	reqStylist = urllib.request.Request('http://models-api:8000/api/v1/stylists/' + str(hair["stylist"]) + '/')
+	resp_jsonStylist = urllib.request.urlopen(reqStylist).read().decode('utf8')
+	respStylist = json.loads(resp_jsonStylist)
+	hair["stylist"] = respStylist["resp"]
+	reqUser = urllib.request.Request('http://models-api:8000/api/v1/users/' + str(hair["stylist"]["user"]) + '/')
+	resp_jsonUser = urllib.request.urlopen(reqUser).read().decode('utf8')
+	respUser = json.loads(resp_jsonUser)
+	hair["stylist"]["user"] = respUser["resp"]
+	reqUser = urllib.request.Request('http://models-api:8000/api/v1/users/' + str(hair["author"]) + '/')
+	resp_jsonUser = urllib.request.urlopen(reqUser).read().decode('utf8')
+	respUser = json.loads(resp_jsonUser)
+	hair["author"] = respUser["resp"]
+
 
 def all_stylists(request):
 	if request.method != 'GET':
@@ -39,6 +62,9 @@ def all_stylists(request):
 		req = urllib.request.Request('http://models-api:8000/api/v1/stylists/all_stylists/')
 		resp_json = urllib.request.urlopen(req).read().decode('utf8')
 		resp = json.loads(resp_json)
+		all_stylists = resp["resp"]["all_stylists"]
+		for stylist in all_stylists:
+			getStylistFK(stylist)
 		return JsonResponse(resp)
 
 def detail_stylist(request, stylist_id):
@@ -48,7 +74,15 @@ def detail_stylist(request, stylist_id):
 		req = urllib.request.Request('http://models-api:8000/api/v1/stylists/' + stylist_id + '/')
 		resp_json = urllib.request.urlopen(req).read().decode('utf8')
 		resp = json.loads(resp_json)
+		stylist = resp["resp"]
+		getStylistFK(stylist)
 		return JsonResponse(resp)
+
+def getStylistFK(stylist):
+	reqStylist = urllib.request.Request('http://models-api:8000/api/v1/users/' + str(stylist["user"]) + '/')
+	resp_jsonStylist = urllib.request.urlopen(reqStylist).read().decode('utf8')
+	respStylist = json.loads(resp_jsonStylist)
+	stylist["user"] = respStylist["resp"]
 
 
 def _error_response(request, error_msg):
--- models/webapp/models.py
+++ models/webapp/models.py
@@ -8,22 +8,26 @@ class User(models.Model):
     password = models.CharField(max_length=96)
     is_active = models.BooleanField()
 
-class Hair(models.Model):
-	location = models.CharField(max_length=100)
-	price = models.DecimalField(max_digits=9, decimal_places=2)
-	hair_phone_number = models.CharField(max_length=15)
-	stylist = models.CharField(max_length=30)
-	hair_upvotes = models.IntegerField(default=0)
-
-class Review(models.Model):
-	title = models.CharField(max_length=30)
-	body = models.TextField()
-	author = models.ForeignKey(User)
-	rating = models.IntegerField(default=0)
-	review_upvotes =models.IntegerField(default=0)
-
 class Stylist(models.Model):
 	stylist_phone_number = models.CharField(max_length=15)
 	years_experience = models.IntegerField(default=0)
 	location = models.CharField(max_length=100)
 	client_gender = models.CharField(max_length=1)
+	user = models.ForeignKey(User)
+
+class Hair(models.Model):
+	location = models.CharField(max_length=100)
+	price = models.DecimalField(max_digits=9, decimal_places=2)
+	hair_phone_number = models.CharField(max_length=15)
+	stylist = models.ForeignKey(Stylist)
+	hair_upvotes = models.IntegerField(default=0)
+	author = models.ForeignKey(User)
+	name = models.CharField(max_length=100)
+
+class Review(models.Model):
+	title = models.CharField(max_length=30)
+	body = models.TextField()
+	rating = models.IntegerField(default=0)
+	review_upvotes = models.IntegerField(default=0)
+	author = models.ForeignKey(User)
+	stylist = models.ForeignKey(Stylist)
