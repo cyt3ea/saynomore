21a59f0075a074aca850a327ac24c2a4733e454f
Christine Tsou <cyt3ea@virginia.edu>
2016-10-23 03:19:40 -0400

    Finished create hair

--- web/frontend/views.py
+++ web/frontend/views.py
@@ -10,17 +10,17 @@ import urllib.parse
 import json
 
 def index(request):
-	if request.method != 'GET':
-		return _error_response(request, 'Must be GET request')
-	else:
-		reqHair = urllib.request.Request('http://exp-api:8000/api/v1/all_hairs/')
-		resp_jsonHair = urllib.request.urlopen(reqHair).read().decode('utf8')
-		respHair = json.loads(resp_jsonHair)['resp']
-		# return JsonResponse(resp)
-		reqStylists = urllib.request.Request('http://exp-api:8000/api/v1/all_stylists/')
-		resp_jsonStylists = urllib.request.urlopen(reqStylists).read().decode('utf8')
-		respStylists = json.loads(resp_jsonStylists)['resp']
-		return render(request, 'frontend/index.html', {'respHair': respHair, 'respStylists': respStylists})
+	# if request.method != 'GET':
+	# 	return _error_response(request, 'Index: Must be GET request')
+	# else:
+	reqHair = urllib.request.Request('http://exp-api:8000/api/v1/all_hairs/')
+	resp_jsonHair = urllib.request.urlopen(reqHair).read().decode('utf8')
+	respHair = json.loads(resp_jsonHair)['resp']
+	# return JsonResponse(resp)
+	reqStylists = urllib.request.Request('http://exp-api:8000/api/v1/all_stylists/')
+	resp_jsonStylists = urllib.request.urlopen(reqStylists).read().decode('utf8')
+	respStylists = json.loads(resp_jsonStylists)['resp']
+	return render(request, 'frontend/index.html', {'respHair': respHair, 'respStylists': respStylists})
 
 def hair_detail(request, hair_id):
 	if request.method != 'GET':
@@ -69,54 +69,54 @@ def create_hair(request):
 			messages.error(request, 'Error creating hair, please try again.')
 			return render(request, 'frontend/create_hair.html', {'form': form})
 	else:
-		return _error_response(request, 'Must be GET request')
+		return _error_response(request, 'Must be POST or GET request')
 	return render(request, 'frontend/create_hair.html', {'form': form})
 
-def create_user(request):
-	if request.method == 'GET':
-		form = UserForm()
-	elif request.method == 'POST':
-		#create a form instance and populate it with data from the request:
-		form = UserForm(request.POST)
-		# check whether it's valid:
-		if form.is_valid():
-			firstname = form.cleaned_data['firstname']
-			lastname = form.cleaned_data['lastname']
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password']
+# def create_user(request):
+# 	if request.method == 'GET':
+# 		form = UserForm()
+# 	elif request.method == 'POST':
+# 		#create a form instance and populate it with data from the request:
+# 		form = UserForm(request.POST)
+# 		# check whether it's valid:
+# 		if form.is_valid():
+# 			firstname = form.cleaned_data['firstname']
+# 			lastname = form.cleaned_data['lastname']
+# 			username = form.cleaned_data['username']
+# 			password = form.cleaned_data['password']
 
-			userdata = {'firstname':firstname, 'lastname': lastname, 'username':username, 'password':password}
-			r = requests.post('http://exp-api:8000/api/v1/create_user/', data=userdata)
+# 			userdata = {'firstname':firstname, 'lastname': lastname, 'username':username, 'password':password}
+# 			r = requests.post('http://exp-api:8000/api/v1/create_user/', data=userdata)
 
-			return HttpResponse("Account has been created!")
+# 			return HttpResponse("Account has been created!")
 			
-	else:
-		return _error_response(request, 'Must be POST or GET request')
-	return render(request, 'frontend/create_user.html', {'form': form})
+# 	else:
+# 		return _error_response(request, 'Must be POST or GET request')
+# 	return render(request, 'frontend/create_user.html', {'form': form})
 
 #Check login through all the layers
-def login(request):
-	f = LoginForm(request.POST)
-	if request.method == 'GET':
-		next = request.GET.get('index') or reverse('LoginForm')
-		return render(request, 'frontend/login.html', {'form': f})
+# def login(request):
+# 	f = LoginForm(request.POST)
+# 	if request.method == 'GET':
+# 		next = request.GET.get('index') or reverse('LoginForm')
+# 		return render(request, 'frontend/login.html', {'form': f})
 	
-	if not f.is_valid():
-		return render(request, 'frontend/login.html', {'form': f})
-	username = f.cleaned_data['username']
-	password = f.cleaned_data['password']
-	next = f.cleaned_data.get('index') or reverse('LoginForm')
+# 	if not f.is_valid():
+# 		return render(request, 'frontend/login.html', {'form': f})
+# 	username = f.cleaned_data['username']
+# 	password = f.cleaned_data['password']
+# 	next = f.cleaned_data.get('index') or reverse('LoginForm')
 	
-	#CALL EXP LAYER BELOW
-	resp = urllib.request.Request('http://exp-api:8000/api/v1/login-exp/')
-	resp_jsonLogin = urllib.request.urlopen(resp).read().decode('utf8')
-	respLogin = json.loads(resp_jsonLogin)['resp']
-	if not resp or not resp['ok']:
-		return render(request, 'frontend/login.html', {'form': f})
-	authenticator = resp['resp']['authenticator']
-	response = HttpResponseRedirect(next)
-	response.set_cookie("auth", authenticator)
-	return response
+# 	#CALL EXP LAYER BELOW
+# 	resp = urllib.request.Request('http://exp-api:8000/api/v1/login-exp/')
+# 	resp_jsonLogin = urllib.request.urlopen(resp).read().decode('utf8')
+# 	respLogin = json.loads(resp_jsonLogin)['resp']
+# 	if not resp or not resp['ok']:
+# 		return render(request, 'frontend/login.html', {'form': f})
+# 	authenticator = resp['resp']['authenticator']
+# 	response = HttpResponseRedirect(next)
+# 	response.set_cookie("auth", authenticator)
+# 	return response
 
 def _error_response(request, error_msg):
 	return JsonResponse({'ok': False, 'error': error_msg})
