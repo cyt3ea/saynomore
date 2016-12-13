da9ff7e05715115f65ebc2e3fd8b9d61eeaecb29
Christine Tsou <cyt3ea@virginia.edu>
2016-12-13 01:29:01 -0500

    updated sparky

--- sparky/spark.py
+++ sparky/spark.py
@@ -3,17 +3,12 @@ import itertools
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
-data = sc.textFile("/app/access2.log", 2)     # each worker loads a piece of the data file
+data = sc.textFile("/app/access3.log", 2)
 
-pairs = data.map(lambda line: line.split("\t"))   # tell each worker to split each line of it's partition
-key_value = pairs.groupByKey()
-# key_value = key_value.values().distinct()
-# key_value = key_value.distinct()
-# pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
-count = key_value.reduceByKey(lambda x,y: x+y)        # shuffle the data so that each key is only on one worker
-                                                  # and then reduce all the values by adding them together
+pairs = data.map(lambda line: line.split("\t"))
+count = pairs.groupByKey()
 
-output = count.distinct().mapValues(list).collect()                          # bring the data back to the master node so we can print it out
+output = count.distinct().mapValues(list).collect()
 
 all_pairs = []
 f = open('/app/output.log', 'w+')
@@ -31,9 +26,7 @@ output = count.collect()
 
 for pair, count in output:
 	print(str(pair) + " " + str(count))
-	f.write(str(pair) + "\t" + str(count) + "\n")
-    # print (co_view + " " + str(count))
-    # f.write("co_view %s count %d\n" % (co_view, count))  
+	f.write(str(pair) + "\t" + str(count) + "\n") 
 print ("Popular items done")
 f.close()
 
