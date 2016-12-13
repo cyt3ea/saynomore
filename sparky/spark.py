4b9ceae4ea997752bb2320aed97638b70215e1da
Christine Tsou <cyt3ea@virginia.edu>
2016-12-13 15:22:13 -0500

    we love you iannnn

--- sparky/spark.py
+++ sparky/spark.py
@@ -1,33 +1,31 @@
 from pyspark import SparkContext
 import itertools
 
-sc = SparkContext("spark://spark-master:7077", "PopularItems")
+sc = SparkContext("spark://spark-master:7077", "RecommendedItems")
 
-data = sc.textFile("/app/access3.log", 2)
+data = sc.textFile("/app/access2.log", 2)
 
-pairs = data.map(lambda line: line.split("\t"))
-count = pairs.groupByKey()
+lines = data.map(lambda line: line.split("\t"))
+user_clicks = lines.groupByKey()
+pairs = user_clicks.flatMap(lambda pair: ((pair[0], x) for x in itertools.combinations(set(pair[1]), 2)))
 
-output = count.distinct().mapValues(list).collect()
+# for pair, count in pairs.collect():
+# 	print(str(pair) + " " + str(list(count)))
 
-all_pairs = []
-f = open('/app/output.log', 'w+')
-for co_view, item_list in output:
-	items = list(set(item_list))
-	combinations = list(itertools.combinations(items, 2))
-	all_pairs = all_pairs + combinations
+pair_users = pairs.distinct().groupBy(lambda pair: pair[1])
 
-print(all_pairs)
-rdd = sc.parallelize(all_pairs)
-pairs = rdd.map(lambda pair: (pair, 1))
-count = pairs.reduceByKey(lambda x,y: x+y)
+# for pair, count in pair_users.collect():
+# 	print(str(pair) + " " + str(list(count)))
+
+count = pair_users.map(lambda pair: (pair[0], len(pair[1])))
 count = count.filter(lambda x: x[1] > 2)
 output = count.collect()
 
+f = open('/app/output.log', 'w+')
 for pair, count in output:
 	print(str(pair) + " " + str(count))
 	f.write(str(pair) + "\t" + str(count) + "\n") 
-print ("Popular items done")
+print ("Recommended items done")
 f.close()
 
 sc.stop()
